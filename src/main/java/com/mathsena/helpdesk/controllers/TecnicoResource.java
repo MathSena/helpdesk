430f76b1ce3e3a1341a953181433d18ef3aff0db
Matheus Sena <MathSena07@hotmail.com>
2022-10-10 16:09:36 -0300

    Padrão DTO para tecnico

--- src/main/java/com/mathsena/helpdesk/controllers/TecnicoResource.java
+++ src/main/java/com/mathsena/helpdesk/controllers/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.mathsena.helpdesk.controllers;
 
 import com.mathsena.helpdesk.domain.Tecnico;
+import com.mathsena.helpdesk.domain.dtos.TecnicoDTO;
 import com.mathsena.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value="/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
 
     }
 }
