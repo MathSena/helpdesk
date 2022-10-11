6860b2eaa6011793f21f88ce8e54c02f41da5f8d
Matheus Sena <MathSena07@hotmail.com>
2022-10-11 18:33:54 -0300

    Criando endpoint de busca de tecnicos

--- src/main/java/com/mathsena/helpdesk/controllers/TecnicoResource.java
+++ src/main/java/com/mathsena/helpdesk/controllers/TecnicoResource.java
@@ -10,17 +10,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping(value="/tecnicos")
+@RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
 
     @Autowired
     private TecnicoService service;
 
-    @GetMapping(value="/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<Tecnico> list = service.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(TecnicoDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
 
     }
 }
